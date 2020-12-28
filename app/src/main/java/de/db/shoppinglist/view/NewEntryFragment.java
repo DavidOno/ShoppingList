14464005fda7d39d394ef41b74e9f4fd0ac5482c
DavidOno <DavidOno@users.noreply.github.com>
2020-12-28 02:28:07 +0100

    First Attempt to add CameraFragment

--- app/src/main/java/de/db/shoppinglist/view/NewEntryFragment.java
+++ app/src/main/java/de/db/shoppinglist/view/NewEntryFragment.java
@@ -1,17 +1,24 @@
 package de.db.shoppinglist.view;
 
 import android.content.Context;
+import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -19,7 +26,11 @@ import androidx.core.view.MenuCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
 import androidx.navigation.fragment.NavHostFragment;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import de.db.shoppinglist.R;
 import de.db.shoppinglist.model.EntryHistoryElement;
@@ -33,6 +44,7 @@ public class NewEntryFragment extends Fragment {
     private EditText quantityEditText;
     private EditText unitOfQuantityEditText;
     private EditText detailsEditText;
+    private ImageView image;
     private ShoppingList list;
     private EntryHistoryElement entry;
     private String entryName;
@@ -50,8 +62,103 @@ public class NewEntryFragment extends Fragment {
         }else{
             nameOfProductEditText.setText(entryName);
         }
+        image.setOnClickListener(v -> inflatePopupMenu());
         return view;
     }
+
+    private void inflatePopupMenu() {
+        PopupMenu popup = new PopupMenu(getContext(), image);
+        popup.getMenuInflater()
+                .inflate(R.menu.popup_menu, popup.getMenu());
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(
+                        getContext(),
+                        "You Clicked : " + item.getTitle(),
+                        Toast.LENGTH_SHORT
+                ).show();
+//                handleCamera();
+                NavController navController = NavHostFragment.findNavController(NewEntryFragment.this);
+                NavDirections navDirections = NewEntryFragmentDirections.actionNewEntryFragmentToCameraFragment();
+                navController.navigate(navDirections);
+                return true;
+            }
+        });
+
+        popup.show();
+    }
+
+//    /** Check if this device has a camera */
+//    private boolean checkCameraHardware(Context context) {
+//        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+//            // this device has a camera
+//            return true;
+//        } else {
+//            // no camera on this device
+//            return false;
+//        }
+//    }
+//
+//    /** A safe way to get an instance of the Camera object. */
+//    public static Camera getCameraInstance(){
+//        Camera c = null;
+//        try {
+//            c = Camera.open(); // attempt to get a Camera instance
+//        }
+//        catch (Exception e){
+//            // Camera is not available (in use or does not exist)
+//        }
+//        return c; // returns null if camera is unavailable
+//    }
+//
+//
+//
+//    private void handleCamera() {
+//        android.graphics.Camera camera = android.graphics.Camera.
+//    }
+
+//    public void takePictureNoPreview(Context context) throws IOException {
+//        // open back facing camera by default
+//        Camera myCamera = Camera.open();
+//
+//        if (myCamera != null) {
+//            try {
+//                //set camera parameters if you want to
+//                //...
+//
+//                // here, the unused surface view and holder
+//                SurfaceView dummy = new SurfaceView(context)
+//                myCamera.setPreviewDisplay(dummy.getHolder());
+//                myCamera.startPreview();
+//
+//                myCamera.takePicture(null, null, getJpegCallback());
+//
+//            } finally {
+//                myCamera.close();
+//            }
+//
+//        } else {
+//            //booo, failed!
+//        }
+//    }
+//
+//        private Camera.PictureCallback getJpegCallback () {
+//            Camera.PictureCallback jpeg = new Camera.PictureCallback() {
+//                @Override
+//                public void onPictureTaken(byte[] data, Camera camera) {
+//                    FileOutputStream fos;
+//                    try {
+//                        fos = new FileOutputStream("test.jpeg");
+//                        fos.write(data);
+//                        fos.close();
+//                    } catch (IOException e) {
+//                        //do something about it
+//                    }
+//                }
+//            };
+//            return null;
+//        }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -168,5 +275,6 @@ public class NewEntryFragment extends Fragment {
         quantityEditText = view.findViewById(R.id.quantityEditText);
         unitOfQuantityEditText = view.findViewById(R.id.unitOfQuantityEditText);
         detailsEditText = view.findViewById(R.id.detailsEditText);
+        image = view.findViewById(R.id.new_entry_imageView);
     }
 }
